217a892f7c095a16a8e6148c04b325fd8307a6a7
hirooka <dev@hirooka.pro>
2016-01-17 21:07:28 +0900

    [add] 直近の EPG 取得から所定の時間経過していない場合，起動時に EPG を取得しない設定の追加 (コミット漏れ)

---
+++ src/main/java/pro/hirooka/chukasa/domain/LastEPGCrawlerExecuted.java
@@ -0,0 +1,14 @@
+package pro.hirooka.chukasa.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+public class LastEPGCrawlerExecuted {
+    //@Id
+    private int unique;
+    private long date;
+}
+
+
+
---
+++ src/main/java/pro/hirooka/chukasa/service/ILastEPGCrawlerExecutedService.java
@@ -0,0 +1,10 @@
+package pro.hirooka.chukasa.service;
+
+import pro.hirooka.chukasa.domain.LastEPGCrawlerExecuted;
+
+public interface ILastEPGCrawlerExecutedService {
+    LastEPGCrawlerExecuted create(LastEPGCrawlerExecuted lastEPGCrawlerExecuted);
+    LastEPGCrawlerExecuted read(int unique);
+    LastEPGCrawlerExecuted update(LastEPGCrawlerExecuted lastEPGCrawlerExecuted);
+    void delete(int unique);
+}
