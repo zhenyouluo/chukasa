167953df7cdf74a3bf48a9d8dd5d2dea726b43b5
hirooka <dev@hirooka.pro>
2016-03-05 14:28:43 +0900

    [add] セルラー用のトランスコード設定を追加

--- src/main/java/pro/hirooka/chukasa/domain/type/VideoResolutionType.java
+++ src/main/java/pro/hirooka/chukasa/domain/type/VideoResolutionType.java
@@ -2,7 +2,10 @@ package pro.hirooka.chukasa.domain.type;
 
 public enum VideoResolutionType {
     FULL_HD("1920x1080"),
-    HD("1280x720");
+    HD("1280x720"),
+    WIFI_HIGH("960x540"),
+    WIFI_LOW("640x360"),
+    CELLULAR("400x224");
 
     private final String name;
 
