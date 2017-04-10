4b4edcfa7dc8d0157af5656b42638b7cd65ee0e2
hirooka <dev@hirooka.pro>
2017-04-11 00:15:36 +0900

    [modify] epgdump + recxxx

--- src/main/java/pro/hirooka/chukasa/domain/service/epgdump/runner/EpgdumpRunnerService.java
+++ src/main/java/pro/hirooka/chukasa/domain/service/epgdump/runner/EpgdumpRunnerService.java
@@ -11,16 +11,12 @@ import pro.hirooka.chukasa.domain.configuration.EpgdumpConfiguration;
 import pro.hirooka.chukasa.domain.configuration.SystemConfiguration;
 import pro.hirooka.chukasa.domain.model.chukasa.constants.ChukasaConstant;
 import pro.hirooka.chukasa.domain.model.common.TunerStatus;
-import pro.hirooka.chukasa.domain.model.common.enums.RecxxxDriverType;
 import pro.hirooka.chukasa.domain.model.epgdump.LastEpgdumpExecuted;
-import pro.hirooka.chukasa.domain.model.epgdump.RecdvbBSModel;
 import pro.hirooka.chukasa.domain.model.recorder.ChannelConfiguration;
 import pro.hirooka.chukasa.domain.model.recorder.enums.ChannelType;
 import pro.hirooka.chukasa.domain.service.common.ITunerManagementService;
-import pro.hirooka.chukasa.domain.service.common.ulitities.CommonUtilityService;
 import pro.hirooka.chukasa.domain.service.epgdump.ILastEpgdumpExecutedService;
 import pro.hirooka.chukasa.domain.service.epgdump.parser.IEpgdumpParser;
-import pro.hirooka.chukasa.domain.service.epgdump.runner.helper.IEpgdumpRecdvbHelper;
 
 import java.io.*;
 import java.util.Date;
@@ -43,31 +39,34 @@ public class EpgdumpRunnerService implements IEpgdumpRunnerService {
     @Autowired
     private ILastEpgdumpExecutedService lastEpgdumpExecutedService;
     @Autowired
-    private IEpgdumpRecdvbHelper epgdumpRecdvbHelper;
-    @Autowired
     private ITunerManagementService tunerManagementService;
 
     @Async
     @Override
     public Future<Integer> submit(List<ChannelConfiguration> channelConfigurationList) {
 
-        final RecxxxDriverType recxxxDriverType = tunerManagementService.getRecxxxDriverType();
-        TunerStatus tunerStatusGR = tunerManagementService.findOne(ChannelType.GR);
-        tunerStatusGR = tunerManagementService.update(tunerStatusGR, false);
-        TunerStatus tunerStatusBS = tunerManagementService.findOne(ChannelType.BS);
-        tunerStatusBS = tunerManagementService.update(tunerStatusBS, false);
+        // TODO: null
+        final TunerStatus tunerStatusGR = tunerManagementService.findOne(ChannelType.GR);
+        final TunerStatus usingTunerStatusGR = tunerManagementService.update(tunerStatusGR, false);
+        final TunerStatus tunerStatusBS = tunerManagementService.findOne(ChannelType.BS);
+        final TunerStatus usingTunerStatusBS = tunerManagementService.update(tunerStatusBS, false);
 
-        File temporaryEpgdumpPathFile = new File(epgdumpConfiguration.getTemporaryPath());
+        final File temporaryEpgdumpPathFile = new File(epgdumpConfiguration.getTemporaryPath());
+
+        cleanupTemporaryEpgdumpPath(temporaryEpgdumpPathFile);
         if(temporaryEpgdumpPathFile.mkdirs()){
-            //
+            log.info("epgdump temporary path: {}", temporaryEpgdumpPathFile);
         }else{
-            //
+            log.error("cannot create epgdump temporary path: {}", temporaryEpgdumpPathFile);
+            releaseTuner(tunerStatusGR);
+            releaseTuner(tunerStatusBS);
+            return new AsyncResult<>(-1);
         }
-        String epgdumpShell = epgdumpConfiguration.getTemporaryPath() + FILE_SEPARATOR + "epgdump.sh";
+        final String epgdumpShellPath = epgdumpConfiguration.getTemporaryPath() + FILE_SEPARATOR + "epgdump.sh";
 
-        File file = new File(epgdumpShell);
+        final File epgdumpShellFile = new File(epgdumpShellPath);
         try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(epgdumpShellFile));
             bufferedWriter.write("#!/bin/bash");
             bufferedWriter.newLine();
             boolean isBS = false;
@@ -76,20 +75,17 @@ public class EpgdumpRunnerService implements IEpgdumpRunnerService {
                     try {
                         final int physicalLogicalChannel = channelConfiguration.getPhysicalLogicalChannel();
                         final String recxxxCommand;
-                        if(recxxxDriverType == RecxxxDriverType.DVB){
-                            if(channelConfiguration.getChannelType() == ChannelType.GR) {
-                                recxxxCommand = systemConfiguration.getRecxxxPath() + " --dev " + tunerStatusGR.getIndex() + " " + physicalLogicalChannel + " " + epgdumpConfiguration.getRecordingDuration() + " " + epgdumpConfiguration.getTemporaryPath() + FILE_SEPARATOR + "epgdump" + physicalLogicalChannel + ".ts";
-                            }else if(channelConfiguration.getChannelType() == ChannelType.BS){
-                                RecdvbBSModel recdvbBSModel = epgdumpRecdvbHelper.resovle(physicalLogicalChannel);
-                                recxxxCommand = systemConfiguration.getRecxxxPath() + " --dev " + tunerStatusBS.getIndex() + " --tsid " + recdvbBSModel.getTsid() + " " + recdvbBSModel.getName() + " " + epgdumpConfiguration.getRecordingDuration() + " " + epgdumpConfiguration.getTemporaryPath() + FILE_SEPARATOR + "epgdump" + physicalLogicalChannel + ".ts";
-                            }else{
-                                recxxxCommand = "";
-                            }
+                        if(channelConfiguration.getChannelType() == ChannelType.GR) {
+                            recxxxCommand = systemConfiguration.getRecxxxPath() + " --dev " + usingTunerStatusGR.getIndex() + " " + physicalLogicalChannel + " " + epgdumpConfiguration.getRecordingDuration() + " " + epgdumpConfiguration.getTemporaryPath() + FILE_SEPARATOR + "epgdump" + physicalLogicalChannel + ".ts";
+                        }else if(channelConfiguration.getChannelType() == ChannelType.BS){
+                            recxxxCommand = systemConfiguration.getRecxxxPath() + " --dev " + usingTunerStatusBS.getIndex() + " " + physicalLogicalChannel + " " + epgdumpConfiguration.getRecordingDuration() + " " + epgdumpConfiguration.getTemporaryPath() + FILE_SEPARATOR + "epgdump" + physicalLogicalChannel + ".ts";
                         }else{
-                            recxxxCommand = systemConfiguration.getRecxxxPath() + " " + physicalLogicalChannel + " " + epgdumpConfiguration.getRecordingDuration() + " " + epgdumpConfiguration.getTemporaryPath() + FILE_SEPARATOR + "epgdump" + physicalLogicalChannel + ".ts";
-
+                            log.error("unknown ChannelType");
+                            releaseTuner(tunerStatusGR);
+                            releaseTuner(tunerStatusBS);
+                            return new AsyncResult<>(-1);
                         }
-                        String epgdumpCommand = epgdumpConfiguration.getPath() + " json " + epgdumpConfiguration.getTemporaryPath()+ FILE_SEPARATOR + "epgdump" + physicalLogicalChannel + ".ts " + epgdumpConfiguration.getTemporaryPath() + FILE_SEPARATOR + "epgdump" + physicalLogicalChannel + ".json";
+                        final String epgdumpCommand = epgdumpConfiguration.getPath() + " json " + epgdumpConfiguration.getTemporaryPath()+ FILE_SEPARATOR + "epgdump" + physicalLogicalChannel + ".ts " + epgdumpConfiguration.getTemporaryPath() + FILE_SEPARATOR + "epgdump" + physicalLogicalChannel + ".json";
                         bufferedWriter.write(recxxxCommand);
                         bufferedWriter.newLine();
                         bufferedWriter.write(epgdumpCommand);
@@ -103,87 +99,98 @@ public class EpgdumpRunnerService implements IEpgdumpRunnerService {
                 }
             }
             bufferedWriter.close();
+
+            final String[] chmodCommandArray = {"chmod", "755", epgdumpShellPath};
+            executeCommand(chmodCommandArray);
+
+            final long begin = System.currentTimeMillis();
+
+            final String[] epgdumpCommandArray = {epgdumpShellPath};
+            executeCommand(epgdumpCommandArray);
+
+            for(ChannelConfiguration channelConfiguration : channelConfigurationList) {
+                final String jsonStringPath = epgdumpConfiguration.getTemporaryPath() + FILE_SEPARATOR + "epgdump" + channelConfiguration.getPhysicalLogicalChannel() + ".json";
+                if(new File(jsonStringPath).exists()) {
+                    try {
+                        epgdumpParser.parse(jsonStringPath, channelConfiguration.getPhysicalLogicalChannel(), channelConfiguration.getRemoteControllerChannel());
+                    } catch (IOException e) {
+                        log.error("cannot parse epgdump output: {} {}", e.getMessage(), e);
+                        releaseTuner(tunerStatusGR);
+                        releaseTuner(tunerStatusBS);
+                        return new AsyncResult<>(-1);
+                    }
+                }
+            }
+
+            final long end = System.currentTimeMillis();
+            log.info((end - begin) / 1000 + "s");
+
+            final LastEpgdumpExecuted previousLastEpgdumpExecuted = lastEpgdumpExecutedService.read(1);
+            final LastEpgdumpExecuted newLastEpgdumpExecuted;
+            final Date date = new Date();
+            if (previousLastEpgdumpExecuted == null) {
+                newLastEpgdumpExecuted = new LastEpgdumpExecuted();
+                newLastEpgdumpExecuted.setDate(date.getTime());
+                newLastEpgdumpExecuted.setUnique(1);
+            }else{
+                previousLastEpgdumpExecuted.setDate(date.getTime());
+                newLastEpgdumpExecuted = previousLastEpgdumpExecuted;
+            }
+            lastEpgdumpExecutedService.update(newLastEpgdumpExecuted);
+            log.info("lastEpgdumpExecuted = {}", newLastEpgdumpExecuted.getDate());
+
+            cleanupTemporaryEpgdumpPath(temporaryEpgdumpPathFile);
+
+            releaseTuner(tunerStatusGR);
+            releaseTuner(tunerStatusBS);
+            return new AsyncResult<>(0);
+
         } catch (IOException e) {
             log.error("{} {}", e.getMessage(), e);
+            releaseTuner(tunerStatusGR);
+            releaseTuner(tunerStatusBS);
+            return new AsyncResult<>(-1);
         }
-
-        if(true){
-            String[] chmodCommandArray = {"chmod", "755", epgdumpShell};
-            ProcessBuilder processBuilder = new ProcessBuilder(chmodCommandArray);
-            try {
-                Process process = processBuilder.start();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-                String s = "";
-                while((s = bufferedReader.readLine()) != null){
-                    log.info("{}", s);
-                }
-                bufferedReader.close();
-                process.destroy();
-            } catch (IOException e) {
-                log.error("{} {}", e.getMessage(), e);
-            }
-        }
-
-        long begin = System.currentTimeMillis();
-
-        if(true){
-            String[] epgdumpCommandArray = {epgdumpShell};
-            ProcessBuilder processBuilder = new ProcessBuilder(epgdumpCommandArray);
-            try {
-                Process process = processBuilder.start();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-                String s = "";
-                while((s = bufferedReader.readLine()) != null){
-                    log.info("{}", s);
-                }
-                bufferedReader.close();
-                process.destroy();
-            } catch (IOException e) {
-                log.error("{} {}", e.getMessage(), e);
-            }
-        }
-
-        for(ChannelConfiguration channelConfiguration : channelConfigurationList) {
-            String jsonStringPath = epgdumpConfiguration.getTemporaryPath() + FILE_SEPARATOR + "epgdump" + channelConfiguration.getPhysicalLogicalChannel() + ".json";
-            if(new File(jsonStringPath).exists()) {
-                try {
-                    epgdumpParser.parse(jsonStringPath, channelConfiguration.getPhysicalLogicalChannel(), channelConfiguration.getRemoteControllerChannel());
-                } catch (IOException e) {
-                    log.error("{} {}", e.getMessage(), e);
-                    return new AsyncResult<>(-1);
-                }
-            }
-        }
-
-        long end = System.currentTimeMillis();
-        log.info((end - begin) / 1000 + "s");
-
-
-        LastEpgdumpExecuted lastEpgdumpExecuted = lastEpgdumpExecutedService.read(1);
-        if (lastEpgdumpExecuted == null) {
-            lastEpgdumpExecuted = new LastEpgdumpExecuted();
-            lastEpgdumpExecuted.setUnique(1);
-        }
-        Date date = new Date();
-        lastEpgdumpExecuted.setDate(date.getTime());
-        lastEpgdumpExecuted = lastEpgdumpExecutedService.update(lastEpgdumpExecuted);
-        log.info("lastEpgdumpExecuted = {}", lastEpgdumpExecuted.getDate());
-
-        try {
-            FileUtils.cleanDirectory(temporaryEpgdumpPathFile);
-            temporaryEpgdumpPathFile.delete();
-        } catch (IOException e) {
-            log.error("{} {}", e.getMessage(), e);
-        }
-
-        tunerManagementService.update(tunerStatusGR, true);
-        tunerManagementService.update(tunerStatusBS, true);
-
-        return new AsyncResult<>(0);
     }
 
     @Override
     public void cancel() {
 
+    }
+
+    private void executeCommand(String[] commandArray){
+        final ProcessBuilder processBuilder = new ProcessBuilder(commandArray);
+        try {
+            final Process process = processBuilder.start();
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            String s = "";
+            while((s = bufferedReader.readLine()) != null){
+                log.info("{}", s);
+            }
+            bufferedReader.close();
+            process.destroy();
+        } catch (IOException e) {
+            log.error("{} {}", e.getMessage(), e);
+        }
+    }
+
+    private void releaseTuner(TunerStatus tunerStatus){
+        tunerManagementService.update(tunerStatus, true);
+    }
+
+    private boolean cleanupTemporaryEpgdumpPath(File file){
+        try {
+            if(file.exists()) {
+                FileUtils.cleanDirectory(file);
+                if (file.delete()) {
+                    return true;
+                } else {
+                    log.info("cannot delete temporary epgdump path: {}", file.getAbsolutePath());
+                }
+            }
+        } catch (IOException e) {
+            log.info("cannot clean temporary epgdump path:{} {}", e.getMessage(), e);
+        }
+        return false;
     }
 }
