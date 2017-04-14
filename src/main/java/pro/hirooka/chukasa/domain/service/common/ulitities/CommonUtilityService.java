1394c170516c1d21785333caa7a86286138998bd
hirooka <dev@hirooka.pro>
2017-04-14 21:43:39 +0900

    [modify] read custom tuner.json

--- src/main/java/pro/hirooka/chukasa/domain/service/common/ulitities/CommonUtilityService.java
+++ src/main/java/pro/hirooka/chukasa/domain/service/common/ulitities/CommonUtilityService.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import pro.hirooka.chukasa.domain.configuration.ChukasaConfiguration;
@@ -71,9 +72,21 @@ public class CommonUtilityService implements ICommonUtilityService {
 
     @Override
     public List<Tuner> getTunerList() {
+
+        final String path = ICommonUtilityService.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        final String[] pathArray = path.split(FILE_SEPARATOR);
+        String currentPath = "";
+        for(int i = 1; i < pathArray.length - 3; i++){
+            currentPath = currentPath + FILE_SEPARATOR + pathArray[i];
+        }
+        log.info("currentPath = {}", currentPath);
+
         List<Tuner> tunerList = new ArrayList<>();
         try {
-            final Resource resource = new ClassPathResource(commonConfiguration.getTuner());
+            Resource resource = new FileSystemResource(currentPath + FILE_SEPARATOR + commonConfiguration.getTuner());
+            if(!resource.exists()){
+                resource = new ClassPathResource(commonConfiguration.getTuner());
+            }
             final ObjectMapper objectMapper = new ObjectMapper();
             tunerList = objectMapper.readValue(resource.getInputStream(), TunerWrapper.class).getTunerList();
             log.info("tunerList = {}", tunerList.toString());
