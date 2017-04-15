3f2023d4aeaba56f0db5e3d19da2e6fa2d55d77f
hirooka <dev@hirooka.pro>
2017-04-15 12:56:37 +0900

    [add] simple tool

---
+++ tools/update_hls_js.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+TEMPORARY_DIRECTORY="/tmp/mokyu"
+if [ ! -d "$TEMPORARY_DIRECTORY" ]; then
+  mkdir $TEMPORARY_DIRECTORY
+fi
+cd $TEMPORARY_DIRECTORY
+wget https://github.com/video-dev/hls.js/archive/v$1.tar.gz -O v$1.tar.gz
+tar zxvf v$1.tar.gz
+cd hls.js-$1/dist
+cp hls.min.js $2
+cp hls.js.map $2
